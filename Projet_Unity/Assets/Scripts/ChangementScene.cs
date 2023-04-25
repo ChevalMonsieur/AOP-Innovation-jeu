5f9a63b56de134046c7663a089fce147d8470274
Justin Hurson <just.1et2mii@gmail.com>
2023-04-25 21:45:03 +0200

    ajout d'un bout de map et changement physique joueur

--- Projet_Unity/Assets/Scripts/ChangementScene.cs
+++ Projet_Unity/Assets/Scripts/ChangementScene.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
@@ -6,21 +6,58 @@ using UnityEngine.SceneManagement;
 
 public class ChangementScene : MonoBehaviour
 {
-    public int SceneArrivee;
+    [SerializeField]
+    private GameObject cameraa;
+    private Vector3 positionPreMove = Vector3.zero;
+    private Vector3 reference = Vector3.zero;
+    private Vector3 deplacement = Vector3.zero;
+    public bool vertical;
+    private bool move = false;
     public static int[] infos;
-    public void Start()
-    {
-        
-    }
 
-    public static int Getinfo(int info)
-    {
-        return infos[info];
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        LoadAndSaveData.instance.SaveData();
-        SceneManager.LoadScene(SceneArrivee);
-        //Mouvement.AjouterForce(Arrivee[NumeroScene]);
+        positionPreMove = new Vector3(cameraa.transform.position.x, cameraa.transform.position.y, cameraa.transform.position.z);
+        if(collision.CompareTag("Player")) {
+            move=true;
+            if (vertical)
+            {
+                Debug.Log("vertical");
+                if (Player.speedVerti > 0)
+                {
+                    deplacement = new Vector3(cameraa.transform.position.x, cameraa.transform.position.y + 14, cameraa.transform.position.z);
+                }
+                else if (Player.speedVerti < 0)
+                {
+                    deplacement = new Vector3(cameraa.transform.position.x, cameraa.transform.position.y - 14, cameraa.transform.position.z);
+                }
+            }
+            else
+            {
+                if (Player.facingRight)
+                {
+                    deplacement = new Vector3(cameraa.transform.position.x + 25, cameraa.transform.position.y, cameraa.transform.position.z);
+                }
+                else if (!Player.facingRight)
+                {
+                    deplacement = new Vector3(cameraa.transform.position.x - 25, cameraa.transform.position.y, cameraa.transform.position.z);
+                }
+            }
+        } 
+    }
+
+    private void Update()
+    {
+        MoveCheck();
+    }
+
+    private void MoveCheck() {
+        if (move) {
+            cameraa.transform.position = Vector3.SmoothDamp(cameraa.transform.position, deplacement, ref reference, 0.2f);
+            if (Math.Abs(cameraa.transform.position.x-positionPreMove.x)>=25 || Math.Abs(cameraa.transform.position.y-positionPreMove.y)>=14) {
+                move=false;
+            }
+        }
     }
 }
+
---
+++ Projet_Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public Animator animator;
+    public float groundCheckRadius;
+    public LayerMask solLayer;
+    public Transform groundCheck;
+    public static bool isGrounded;
+    public float vitesse;
+    public float forceSaut;
+    public float forceDoubleSaut;
+    public Rigidbody2D rb;
+    public bool doubleSaut;
+    public int dash;
+    public static float speedVerti;
+    private float tempsDash=-1;
+    private float Hori;
+    private float Verti;
+    public static bool facingRight = true;
+    private bool isDoubleJumping;
+    public static Vector2 velocity = Vector2.zero;
+    private bool jumping = false;
+    private void Update()
+    {
+        MovePlayerHori();
+        FacingDirection();
+        isGrounded = IsGrounded();
+        CheckSaut();
+        isDoubleJumping = CheckDoubleSaut();
+        CheckDash();
+        UpdateAnimation();
+        speedVerti = rb.velocity.y;
+    }
+
+    void MovePlayerHori()
+    {
+        float MouvementHori = Input.GetAxis("Horizontal") * vitesse;
+        Vector2 targetvelocity = new(MouvementHori, rb.velocity.y);
+        rb.velocity = Vector2.SmoothDamp(targetvelocity, rb.velocity, ref velocity, 0.05f);
+
+    }
+
+    void FacingDirection()
+    {
+        if (rb.velocity.x > 0.1)
+        {
+            transform.localScale = new Vector3(Convert.ToSingle(-1), Convert.ToSingle(1), Convert.ToSingle(1));
+            facingRight = true;
+        }
+        if (rb.velocity.x < -0.1)
+        {
+            transform.localScale = new Vector3(Convert.ToSingle(1), Convert.ToSingle(1), Convert.ToSingle(1));
+            facingRight = false;
+        }
+    }
+
+    bool IsGrounded() {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, solLayer); 
+    }
+
+    void CheckSaut()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                jumping= true;
+                rb.AddForce(new Vector2(0, forceSaut));
+            }
+            dash = 2;
+            doubleSaut = true;
+        }
+        else
+        {
+            jumping = false;
+        }
+    }
+
+    bool CheckDoubleSaut()
+    {
+        bool res = false;
+        if (doubleSaut)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+            {
+                jumping = true;
+                rb.velocity = new Vector2(0, 0);
+                rb.AddForce(new Vector2(0, forceDoubleSaut));
+                doubleSaut = false;
+                res = true;
+            }
+        }
+        return res;
+    }
+
+    void CheckDash()
+    {
+        if (dash == 2 || dash == 1)
+        {
+            if (Input.GetMouseButtonDown(0) && dash == 2)
+            {
+                Hori = Input.GetAxisRaw("Horizontal");
+                Verti = Input.GetAxisRaw("Vertical");
+                tempsDash = 100;
+                dash = 1;
+            }
+            if (dash == 1)
+            {
+                rb.velocity = new Vector2(0, 0);
+                if (Hori == 0 && Verti == 0)
+                {
+                    if (facingRight)
+                    {
+                        rb.velocity = new Vector2(15, 1);
+                    }
+                    else
+                    {
+                        rb.velocity = new Vector2(-15, 1);
+                    }
+                }
+                else
+                {
+                    if (Math.Abs(Hori) == 1 && Math.Abs(Verti) == 1)
+                    {
+                        rb.velocity = new Vector2(10 * Hori, 8 * Verti);
+                    }
+                    else
+                    {
+                        rb.velocity = new Vector2(15 * Hori, 8 * Verti);
+                    }
+                }
+                tempsDash--;
+            }
+            if (tempsDash == 0)
+            {
+                dash = 0;
+                tempsDash = -1;
+                rb.velocity /= 3;
+            }
+        }
+    }
+
+/*    public static void AjouterForce(Vector2 vector)
+    {
+        (vector);
+    }*/
+
+    void UpdateAnimation()
+    {
+        animator.SetBool("jumping", jumping);
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("speedHori", Math.Abs(rb.velocity.x));
+    }
+}
