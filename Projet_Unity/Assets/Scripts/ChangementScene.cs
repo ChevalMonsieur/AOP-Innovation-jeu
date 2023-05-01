d31519a3547a0439b678351a40eec26304d682c0
Justin Hurson <just.1et2mii@gmail.com>
2023-05-01 19:27:21 +0200

    mini update pour le merge

--- Projet_Unity/Assets/Scripts/ChangementScene.cs
+++ Projet_Unity/Assets/Scripts/ChangementScene.cs
@@ -13,17 +13,16 @@ public class ChangementScene : MonoBehaviour
     private Vector3 reference = Vector3.zero;
     private Vector3 deplacement = Vector3.zero;
     public bool vertical;
-    private bool move = false;
+    //private bool move = false;
     public static int[] infos;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         positionPreMove = new Vector3(cameraa.transform.position.x, cameraa.transform.position.y, cameraa.transform.position.z);
         if(collision.CompareTag("Player")) {
-            move=true;
+            //move=true;
             if (vertical)
             {
-                Debug.Log("vertical");
                 if (Player.speedVerti > 0)
                 {
                     deplacement = new Vector3(cameraa.transform.position.x, cameraa.transform.position.y + 14, cameraa.transform.position.z);
@@ -35,31 +34,32 @@ public class ChangementScene : MonoBehaviour
             }
             else
             {
-                if (Player.facingRight)
+                if (Player.speedHori > 0)
                 {
                     deplacement = new Vector3(cameraa.transform.position.x + 25, cameraa.transform.position.y, cameraa.transform.position.z);
                 }
-                else if (!Player.facingRight)
+                else if (Player.speedHori < 0)
                 {
                     deplacement = new Vector3(cameraa.transform.position.x - 25, cameraa.transform.position.y, cameraa.transform.position.z);
                 }
             }
+            cameraa.transform.position = deplacement;
         } 
     }
 
-    private void Update()
-    {
-        MoveCheck();
-    }
+    //  private void Update()
+    //  {
+    //      MoveCheck();
+    //  }
 
-    private void MoveCheck() {
-        if (move) {
-            cameraa.transform.position = Vector3.SmoothDamp(cameraa.transform.position, deplacement, ref reference, 0.1f);
-            if (Math.Abs(cameraa.transform.position.x-positionPreMove.x)>=24.99 || Math.Abs(cameraa.transform.position.y-positionPreMove.y)>=13.99) {
-                move=false;
-            cameraa.transform.position = Vector3.Lerp(cameraa.transform.position, deplacement, 0.2f);
-            }
-        }
-    }
+    //  private void MoveCheck() {
+    //      if (move) {
+    //          cameraa.transform.position = Vector3.SmoothDamp(cameraa.transform.position, deplacement, ref reference, 0.1f);
+    //          if (Math.Abs(cameraa.transform.position.x-positionPreMove.x)>=24.99 || Math.Abs(cameraa.transform.position.y-positionPreMove.y)>=13.99) {
+    //              move=false;
+    //          cameraa.transform.position = Vector3.Lerp(cameraa.transform.position, deplacement, 0.2f);
+    //          }
+    //      }
+    //  }
 }
 
